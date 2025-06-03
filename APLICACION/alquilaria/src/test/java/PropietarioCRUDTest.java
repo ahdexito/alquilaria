9d120e36a6ed275f194a51ece058d41f0dec1be5
Ahdexito <ags120120@gmail.com>
2025-06-03 09:45:33 +0200

    save

--- APLICACION/alquilaria/src/test/java/PropietarioCRUDTest.java
+++ APLICACION/alquilaria/src/test/java/PropietarioCRUDTest.java
@@ -38,7 +38,7 @@ public class PropietarioCRUDTest {
 		
 		/* Crear nuevo objeto */
 		Propietario propietario  = new Propietario
-			(0, "12345678A", "Pepe", "García", "Gonzalez", "655433211");
+			(0, "12345678A", "Pepe", "García González", "pepe@gmail.com", "655433211");
 		
 		/* Ejecutar sentencia de creación */
 		PropietarioCRUD.crear(conex, propietario);
@@ -46,7 +46,7 @@ public class PropietarioCRUDTest {
 		/* Declarar sentencia de consulta */
 		String query = "SELECT * FROM propietario WHERE dni = '12345678A'";
 		
-		/* Crear objeto ResultSet en base a la consulta */
+		/* Crear objeto ResultSet y ejecutar consulta */
 		ResultSet rs = conex.createStatement().executeQuery(query);
 		
 		/* Verificar que ha habido consulta */
@@ -57,6 +57,81 @@ public class PropietarioCRUDTest {
 		
 		/* Verificar que telefono coincide */
 		assertEquals("655433211", rs.getString("telefono"));
+	}
+	
+	@Test
+	public void testModificar() throws SQLException {
+	
+		/* Crear nuevo objeto */
+		Propietario propietario = new Propietario
+			(0, "98765432R", "Jose", "Pérez Gómez", "jose@gmail.com", "667889000");
+		
+		/* Ejecutar sentencia de creación */
+		PropietarioCRUD.crear(conex, propietario);
+		
+		/* Declarar sentencia de consulta */
+		String query = "SELECT *  FROM propietario WHERE dni = '98765432R'";
+		
+		/* Crear objeto ResultSet y ejecutar consulta */
+		ResultSet rs = conex.createStatement().executeQuery(query);
+	
+		/* Obtener ID del campo obtenido */
+		rs.next();
+		int id = rs.getInt("id");		
+		
+		/* Crear nuevo objeto que modificará el anterior */
+		propietario = new Propietario
+			(id, "98765432R", "Jose Manuel", "Pérez Gómez", "jose@gmail.com", "666555444");
+		
+		/* Ejecutar sentencia de modificación */
+		PropietarioCRUD.modificar(conex, propietario);
+		
+		/* Declarar sentencia de consulta */
+		query = "SELECT * FROM propietario WHERE id = " + id;
+		
+		/* Declarar ResultSet y ejecutar consulta */
+		rs = conex.createStatement().executeQuery(query);
+		
+		rs.next();
+		
+		/* Verificar que el nuevo nombre coincide */
+		assertEquals("Jose Manuel", rs.getString("nombre"));
+		
+		/* Verificar que el nuevo teléfono coincide */
+		assertEquals("666555444", rs.getString("telefono"));
+	}
+	
+	@Test
+	public void testEliminar() throws SQLException {
+		
+		/* Crear nuevo objeto */
+		Propietario propietario = new Propietario
+			(0, "11223344C", "Alberto", "López Rodríguez", "alberto@gmail.com", "665544333");
+		
+		/* Ejecutar sentencia de creación */
+		PropietarioCRUD.crear(conex, propietario);
+		
+		/* Declarar sentencia de consulta */
+		String query = "SELECT * FROM propietario WHERE dni = '11223344C'";
+		
+		/* Crear objeto ResultSet y ejecutar consulta */
+		ResultSet rs = conex.createStatement().executeQuery(query);
+		
+		/* Obtener ID del campo obtenido */
+		rs.next();
+		int id = rs.getInt("id");
+		
+		/* Ejecutar sentencia de eliminación */
+		PropietarioCRUD.eliminar(conex, id);
+		
+		/* Declarar sentencia de consulta */
+		query = "SELECT * FROM propietario WHERE id = " + id;
+		
+		/* Declarar ResultSet y ejecutar consulta */
+		rs = conex.createStatement().executeQuery(query);
+		
+		/* Verificar que la consulta no devuelve nada */
+		assertFalse(rs.next());
 	}
 	
 	@AfterAll
